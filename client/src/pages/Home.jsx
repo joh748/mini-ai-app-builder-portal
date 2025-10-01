b9eedb7038e5015acd098dbd338662fdb954b8c7
joh748 <joh748@aucklanduni.ac.nz>
2025-10-02 04:58:58 +1300

    get rid of commented out codes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -47,11 +47,11 @@ export default function Home() {
         <RequirementsSummary requirements={requirements} />
       </div>
 
-      {/* {requirements &&  */}
+      {requirements && 
       <DndContext>
         <ColorSidebar />
       </DndContext>
-      {/*} */}
+      }
 
 
       {requirements && (
